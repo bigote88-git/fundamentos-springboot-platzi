435f51df72ab8d6977583b77bc7c185c40175f47
djcortez <djcortez@inss.gob.ni>
2022-11-15 10:06:15 -0600

    Inicializando el repositorio

---
+++ src/main/java/com/fundamentosplatzi/fundamentosmv/FundamentosmvApplication.java
@@ -0,0 +1,13 @@
+package com.fundamentosplatzi.fundamentosmv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundamentosmvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundamentosmvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fundamentosplatzi/fundamentosmv/FundamentosmvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fundamentosplatzi.fundamentosmv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundamentosmvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
